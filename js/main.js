baf5600a5943c71833d56f75c1b29eb67149422c
jose.bravo <jose.bravo@neoris.com>
2025-04-15 12:08:03 -0300

    TP 1

---
+++ js/main.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dinamicos = document.querySelectorAll(".contenido-dinamico");
+
+  dinamicos.forEach((dinamico) => {
+    const htmlFile = dinamico.getAttribute("data-html");
+
+    fetch(htmlFile)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al cargar " + htmlFile);
+        }
+        return response.text();
+      })
+      .then((html) => {
+        dinamico.innerHTML = html;
+
+        cargarTarjetas(dinamico);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+});
+
+function cargarTarjetas(dinamicoContainer) {
+  const cardContainers = dinamicoContainer.querySelectorAll(
+    ".grid-container-card"
+  );
+
+  if (cardContainers.length === 0) {
+    return;
+  }
+
+  fetch("helpers/json/data.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error al cargar información del sitio");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      cardContainers.forEach((container) => {
+        const selectedType = container.getAttribute("data-type");
+        const filteredCards = data.filter((card) => card.type === selectedType);
+
+        filteredCards.forEach((card) => {
+          const article = document.createElement("article");
+          article.classList.add("card");
+
+          const cardImg = document.createElement("div");
+          cardImg.classList.add("card-img");
+          const img = document.createElement("img");
+          img.src = card.img;
+          img.alt = card.title;
+          cardImg.appendChild(img);
+
+          const cardContent = document.createElement("div");
+          cardContent.classList.add("card-content");
+          const h3 = document.createElement("h3");
+          h3.textContent = card.title;
+          const p = document.createElement("p");
+          p.textContent = card.description;
+
+          cardContent.appendChild(h3);
+          cardContent.appendChild(p);
+
+          article.appendChild(cardImg);
+          article.appendChild(cardContent);
+
+          container.appendChild(article);
+        });
+      });
+    })
+    .catch((error) => {
+      console.error("Error al cargar las cards:", error);
+    });
+}
